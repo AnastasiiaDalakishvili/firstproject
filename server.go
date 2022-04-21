a6ebf0cc9cf4e5d35aa8b51ab67d05009d3231ff
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-04-21 12:58:43 +0100

    refactor: extract player's name ServerHTTP and pass to processWin

--- server.go
+++ server.go
@@ -16,19 +16,17 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
 	switch r.Method {
 	case http.MethodPost:
 		p.processWin(w, r)
 	case http.MethodGet:
-		p.showScore(w, r)
+		p.showScore(w, player)
 
 	}
 }
 
-func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
-
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
 	score := p.store.GetPlayerScore(player)
 
 	if score == 0 {
