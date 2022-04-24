dc96f654f0f987ab79e0e4ee18e645962d6332ab
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-04-24 08:52:15 +0100

    refactor: add comments

--- league.go
+++ league.go
@@ -6,8 +6,10 @@ import (
 	"io"
 )
 
+// League stores a collection of players.
 type League []Player
 
+// Find tries to return a player from a league.
 func (l League) Find(name string) *Player {
 	for i, p := range l {
 		if p.Name == name {
