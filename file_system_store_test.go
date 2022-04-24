f4c86560fc3453a65cacd9542766a39d586129d4
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-04-24 08:48:23 +0100

    test: store wins for a new player

--- file_system_store_test.go
+++ file_system_store_test.go
@@ -81,6 +81,21 @@ func TestFileSystemStore(t *testing.T) {
 		want := 33
 		assertScoreEquals(t, got, want)
 	})
+
+	t.Run("store wins for new players", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, `[
+        {"Name": "Cleo", "Wins": 10},
+        {"Name": "Chris", "Wins": 33}]`)
+		defer cleanDatabase()
+
+		store := FileSystemPlayerStore{database}
+
+		store.RecordWin("Pepper")
+
+		got := store.GetPlayerScore("Pepper")
+		want := 1
+		assertScoreEquals(t, got, want)
+	})
 }
 
 func assertScoresEquals(t testing.TB, got, want int) {
